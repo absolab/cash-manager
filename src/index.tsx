b613689bdcc65e11822dfa4356cbab4f9fbd0990
Hertz <kljj0650@gmail.com>
2023-07-18 16:01:13 +0900

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +8,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      출력 확인
+    </div>
   </React.StrictMode>
 );
 
