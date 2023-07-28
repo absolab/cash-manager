813dd371889bd1e6996abeaeca3ec15abcaabd92
Hertz <kljj0650@gmail.com>
2023-07-28 15:41:47 +0900

    id 유니크키 추가

--- db/db.sql
+++ db/db.sql
@@ -1,17 +1,18 @@
+-- USE mysql;
+-- CREATE USER 'server'@'%' IDENTIFIED BY 'qwerty123';
+-- GRANT ALL PRIVILEGES ON cash.* TO 'server'@'%';
+
+DROP DATABASE IF EXISTS cash;
+
 CREATE DATABASE cash
     DEFAULT CHARACTER SET utf8mb4
     COLLATE utf8mb4_general_ci;
-
-USE mysql;
-
-CREATE USER 'server'@'%' IDENTIFIED BY 'qwerty123';
-GRANT ALL PRIVILEGES ON cash.* TO 'server'@'%';
 
 USE cash;
 
 CREATE TABLE `TB_USER` (
     `seq` INT AUTO_INCREMENT    PRIMARY KEY,
-    `id`  VARCHAR(255)          NOT NULL,
+    `id`  VARCHAR(255)          UNIQUE KEY,
     `pw`  VARCHAR(255)          NOT NULL
 );
 
